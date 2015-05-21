8bfcbdb07198ce4e033f09578d5a796c99f3179f
Gustav Pursche <gustavpursche@gmail.com>
2015-05-21 20:19:21 +0200

    Cleanup Sequence Code, Preload Videos with offset, so they are ready, when users are reaching the section

--- resources/js/dev/modules/image-sequence.js
+++ resources/js/dev/modules/image-sequence.js
@@ -10,55 +10,71 @@ define(
       var imageSequence = new TimelineLite(),
           container = $container.get( 0 ),
           $images = $container.children( '.image-sequence_figure' ),
-          fadeIn = TweenLite.fromTo( $container, 1, { opacity: 0 }, { opacity: 1 } ),
+          fadeIn = TweenLite.fromTo( $container,
+                                      1,
+                                      {
+                                        opacity: 0
+                                      }, {
+                                        opacity: 1
+                                      } ),
 
+          /* initialize the whole section */
           initSection = function( e ) {
-            $.each( $images, function() {
-              var $figure = $( this ),
-                  $figureChild = $figure.children( 'img, video' ),
-                  media;
+            var $videos = $images.find( 'video' ),
+                initializeVideo = function() {
+                  var $video = $( this ),
+                      media;
 
-              /* start preloading the video */
-              if( $figureChild.is( 'video' ) ) {
-                media = $figureChild.get( 0 );
-                media.load();
-                media.play();
-              }
-            });
+                  /* start preloading the video */
+                  media = $video.get( 0 );
+                  media.load();
+                  media.play();
+                };
+
+            $.each( $videos, initializeVideo );
           },
 
+          /* add animation to a single image */
           addImageAnimation = function() {
             var $figure = $( this ),
-                $figureChild = $figure.children( 'img, video' ).eq( 0 ),
                 $nextFigure = $figure.next( '.image-sequence_figure' ),
                 $caption = $figure.children( '.image-sequence_caption' ),
                 imageAnimation = new TimelineLite(),
                 captionLeft = ( $figure.parent().outerWidth() - $caption.outerWidth() ) / 2,
-                captionFadeIn,
-                captionFadeOut;
+                captionFadeIn = TweenLite.to( $caption,
+                                              .8,
+                                              {
+                                                opacity: 1,
+                                              } ),
+                captionFadeOut = TweenLite.to( $caption,
+                                               .8,
+                                                {
+                                                  opacity: 0,
+                                                } ),
+                imageFadeIn = TweenLite.fromTo( $nextFigure,
+                                                1,
+                                                {
+                                                  opacity: 0,
+                                                }, {
+                                                  opacity: 1,
+                                                } ),
+                imageFadeOut = TweenLite.to( $figure,
+                                             1,
+                                             {
+                                               opacity: 0,
+                                             } );
 
             if( $caption.length ) {
               $caption.css({
                 left: captionLeft,
               });
 
-              captionFadeIn = TweenLite.to( $caption,
-                                            .8,
-                                            {
-                                              opacity: 1,
-                                            } );
-
-              captionFadeOut = TweenLite.to( $caption,
-                                             .8,
-                                              {
-                                                opacity: 0,
-                                              } );
-
               imageAnimation
                 .add( captionFadeIn );
 
+              /* pause for a short time */
               imageAnimation
-                .set( {}, {}, '+=.5' );
+                .set( {}, {}, '+=.2' );
 
               imageAnimation
                .add( captionFadeOut );
@@ -68,18 +84,8 @@ define(
             if( $nextFigure.length ) {
               imageAnimation
                 .add([
-                  TweenLite.to( $figure,
-                                1,
-                                {
-                                  opacity: 0,
-                                } ),
-                  TweenLite.fromTo( $nextFigure,
-                                   1,
-                                   {
-                                    opacity: 0,
-                                   }, {
-                                    opacity: 1,
-                                  } ),
+                  imageFadeOut,
+                  imageFadeIn,
                 ]);
             }
 
@@ -89,21 +95,23 @@ define(
 
       $.each( $images, addImageAnimation );
 
+      /* fadeIn the whole section */
       new ScrollMagic.Scene({
           triggerElement: container,
           triggerHook: 'onEnter',
           duration: '100%',
           offset: 150,
       }).setTween( fadeIn )
+        .on( 'enter', initSection )
         .addTo( new ScrollMagic.Controller() );
 
+      /* handle blend of sequence-elements */
       new ScrollMagic.Scene({
           triggerElement: container,
           triggerHook: 'onLeave',
           duration: '200%',
       }).setPin( container, { spacerClass: 'sm-image-sequencer' } )
         .setTween( imageSequence )
-        .on( 'enter', initSection )
         .addTo( new ScrollMagic.Controller() );
     };
 
