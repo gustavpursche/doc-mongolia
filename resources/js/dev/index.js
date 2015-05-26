6498b31debf49398d2ca194bd856b02c1bbe84fa
Gustav Pursche <gustavpursche@gmail.com>
2015-05-26 21:53:56 +0200

    Implement lightbox.js as part of the dynamic module loader system

--- resources/js/dev/index.js
+++ resources/js/dev/index.js
@@ -98,9 +98,12 @@ require( [ 'jquery', ], function( $ ) {
           return;
         }
 
-        require( [ 'modules/' + module ], function( module ) {
-          module( $section, options );
+        $.each( module.split(','), function loadModule( index, localModule ) {
+          require( [ 'modules/' + localModule ], function( localModule ) {
+            localModule( $section, options );
+          });
         });
+
       };
 
   $(function() {
@@ -145,74 +148,4 @@ require( [
       });
     }
   });
-
-});
-
-require( [
-    'jquery',
-    'colorbox',
-    ], function( $ ) {
-  var buildSlickHtml = function( $container ) {
-        var $images = $container.find( '.figure' ),
-            $list = $( '<ul/>' ),
-
-            addListElement = function( index, el ) {
-              var $listEl = $( '<li/>' ),
-                  $image = $( this ).children( 'img' ).clone();
-
-              $listEl.append( $image );
-
-              $list.append( $listEl );
-            };
-
-        $.each( $images, addListElement );
-
-        return $( '<div/>' ).append( $list ).html();
-      },
-
-      initLightbox = function( $container ) {
-        var $trigger = $container.find( '.figure_button' ),
-            lbOpenCallback = function() {
-
-              console.log('lightbox open')
-
-              require( [
-                'slick',
-                ], function() {
-                //FIXME: This currently returns the element, colorbox is associated with. Instead we must get the box-container here, which is wrapped around the content of the lightbox.
-                var $box = $.colorbox.element();
-
-                /* Initialize Slick */
-                $box.slick({
-                  fade: true,
-                  infinite: true,
-                  slide: 'li',
-                  slidesToScroll: 1,
-                  slidesToShow: 1,
-                });
-              });
-            },
-
-            openLightbox = function( e ) {
-              e.preventDefault();
-
-              var html = buildSlickHtml( $container );
-
-              $.colorbox({
-                height: '60%',
-                html: html,
-                width: '60%',
-              });
-
-              $( document ).on( 'cbox_complete', lbOpenCallback );
-            };
-
-        /* Open Lightbox */
-        $trigger.on( 'click', openLightbox );
-      };
-
-  $.each( $( '.slow-scroll-col' ), function( index, el ) {
-    initLightbox( $( el ) );
-  });
-
 });
--- resources/js/dev/modules/lightbox.js
+++ resources/js/dev/modules/lightbox.js
@@ -1,13 +1,66 @@
 define(
   [
     'jquery',
+    'colorbox',
   ],
 
-  function() {
-    var init = function( $container ) {
+  function( $ ) {
+    var buildSlickHtml = function( $container ) {
+          var $images = $container.find( '.figure' ),
+              $list = $( '<ul/>' ),
 
-    };
+              addListElement = function( index, el ) {
+                var $listEl = $( '<li/>' ),
+                    $image = $( this ).children( 'img' ).clone();
 
-    return init;
+                $listEl.append( $image );
+
+                $list.append( $listEl );
+              };
+
+          $.each( $images, addListElement );
+
+          return $( '<div/>' ).append( $list ).html();
+        },
+
+        initLightbox = function( $container, options ) {
+          var $trigger = $container.find( '.figure_button' ),
+              lbOpenCallback = function() {
+                require( [
+                  'slick',
+                  ], function() {
+                  //FIXME: This currently returns the element, colorbox is associated with. Instead we must get the box-container here, which is wrapped around the content of the lightbox.
+                  var $box = $.colorbox.element();
+
+                  /* Initialize Slick */
+                  $box.slick({
+                    fade: true,
+                    infinite: true,
+                    slide: 'li',
+                    slidesToScroll: 1,
+                    slidesToShow: 1,
+                  });
+                });
+              },
+
+              openLightbox = function( e ) {
+                e.preventDefault();
+
+                var html = buildSlickHtml( $container );
+
+                $.colorbox({
+                  height: '60%',
+                  html: html,
+                  width: '60%',
+                });
+
+                $( document ).on( 'cbox_complete', lbOpenCallback );
+              };
+
+          /* Open Lightbox */
+          $trigger.on( 'click', openLightbox );
+        };
+
+    return initLightbox;
   }
-)
+);
